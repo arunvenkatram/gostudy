7a7e9bc4796316bc48c770929ec90476d03071c7
arunvenkatram <arun92venkat@gmail.com>
2019-10-13 14:26:08 +0000

    Done_for_the_day

--- testing/hello_test.go
+++ testing/hello_test.go
@@ -7,6 +7,8 @@ func TestHello(t *testing.T) {
 	emptyResult := hello("")
 	if emptyResult != "Welcome Boss" {
 		t.Errorf("hello(\"\") failed, expected %v, got %v", "Welcome Boss", emptyResult)
+	} else {
+		t.Logf("empty string test succeeded, got expected output \"%v\"", emptyResult)
 	}
 	result := hello("testuser")
 	if result != "Welcome testuser" {
